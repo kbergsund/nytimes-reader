e8425ad671d3d577db05a58a63ea96b56caa4397
Kyra Bergsund <k.bergsund@gmail.com>
2022-03-04 12:03:09 -0800

    Implement filtering by category

--- src/components/StoryContainer.js
+++ src/components/StoryContainer.js
@@ -1,16 +1,27 @@
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import StoryCard from './StoryCard';
 import StoryFilterDropdown from './StoryFilterDropdown';
 import '../css/StoryContainer.css';
 
 const StoryContainer = ({ stories, sections }) => {
+  const [filteredStories, setFilteredStories] = useState([])
+  
+  const filterStories = (section) => {
+    const matchingStories = stories.filter(story => story.section === section)
+    console.log(matchingStories)
+    setFilteredStories(stories.filter(story => story.section === section))
+    return filteredStories.map(story => <StoryCard key={story.id} id={story.id} title={story.title} byline={story.byline} image={story.image} alt={story.imageCaption}/>)
+  }
 
   const memoizeStoryCards = useMemo(() => stories.map(story => <StoryCard key={story.id} id={story.id} title={story.title} byline={story.byline} image={story.image} alt={story.imageCaption}/>), [stories])
+
+  const generateFilteredStories = filteredStories.map(story => <StoryCard key={story.id} id={story.id} title={story.title} byline={story.byline} image={story.image} alt={story.imageCaption}/>)
+
   
   return (
     <div className='story-container'>
-      <StoryFilterDropdown sections={sections}/>
-      {memoizeStoryCards}
+      <StoryFilterDropdown sections={sections} filterStories={filterStories}/>
+      {!filteredStories.length ? memoizeStoryCards : generateFilteredStories}
     </div>
   )
 }
--- src/components/StoryFilterDropdown.js
+++ src/components/StoryFilterDropdown.js
@@ -1,13 +1,19 @@
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 
-const StoryFilterDropdown = ({ sections }) => {
-  const memoizeSectionsOptions = useMemo(() => sections.map(section => <option>{section}</option>), [sections])
-  
-  return(
-    <select>
-      <option disabled>Category</option>
-      {memoizeSectionsOptions}
-    </select>
+const StoryFilterDropdown = ({ sections, filterStories }) => {
+  const [selectedSection, setSelectedSection] = useState('Category')
+
+  const memoizeSectionsOptions = useMemo(() => sections.map((section, index) => <option key={index}>{section}</option>), [sections])
+
+  return (
+    <>
+      <select value={selectedSection} onChange={(e) => setSelectedSection(e.target.value)}>
+        <option disabled>Category</option>
+        {memoizeSectionsOptions}
+      </select>
+      <button onClick={() => filterStories(selectedSection)}>Filter</button>
+      <button onClick={() => filterStories('')}>Back to All Stories</button>
+    </>
   )
 }
 
