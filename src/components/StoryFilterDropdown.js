9a917cbca58a6ec47762cc3a53cf711e911f6174
Kyra Bergsund <k.bergsund@gmail.com>
2022-03-04 12:05:02 -0800

    Style dropdown component

--- src/components/StoryFilterDropdown.js
+++ src/components/StoryFilterDropdown.js
@@ -1,4 +1,5 @@
 import { useMemo, useState } from 'react';
+import '../css/StoryFilterDropdown.css';
 
 const StoryFilterDropdown = ({ sections, filterStories }) => {
   const [selectedSection, setSelectedSection] = useState('Category')
@@ -6,14 +7,14 @@ const StoryFilterDropdown = ({ sections, filterStories }) => {
   const memoizeSectionsOptions = useMemo(() => sections.map((section, index) => <option key={index}>{section}</option>), [sections])
 
   return (
-    <>
+    <section className='story-filter-dropdown'>
       <select value={selectedSection} onChange={(e) => setSelectedSection(e.target.value)}>
         <option disabled>Category</option>
         {memoizeSectionsOptions}
       </select>
       <button onClick={() => filterStories(selectedSection)}>Filter</button>
       <button onClick={() => filterStories('')}>Back to All Stories</button>
-    </>
+    </section>
   )
 }
 
